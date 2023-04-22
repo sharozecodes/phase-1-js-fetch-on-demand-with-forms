0f832cf52237e48d09a2f78fcb3acb611e290289
Sharoze Archer <sharoze.archer@gmail.com>
2023-04-21 20:24:33 -0400

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 const init = () => {
+
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = event.target.searchByID.value;
+
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+
+    })
   
 }
 
